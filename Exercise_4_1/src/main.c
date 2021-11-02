637be7cafdc0d520b372a6e123d0726347c4a1d2
b-lind <himbeerhacks@outlook.de>
2021-11-02 19:05:38 +0100

    Finished Labor 4.1, started Labor 4.2

--- Exercise_4_1/src/main.c
+++ Exercise_4_1/src/main.c
@@ -1,4 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+#define STR_LEN 100
+
+int stringlen(char *sIn);
+
+int stringlen(char *sIn) {
+    int i;
+
+    for(i = 0; i < STR_LEN; i++) {
+        // includes trailing newline
+        if(*(sIn+i) == 0) // || *(sIn+i) == 10)
+            return i;
+    }
+
+    return -1;
+}
 
 int main(int argc, char *argv[]){
+    
+    char sInput[STR_LEN] = "";
 
+    printf("Input: ");
+    
+    fgets(sInput, STR_LEN, stdin);
+
+    printf("Länge: %d\n", stringlen(sInput));
 }
--- Exercise_4_2/src/main.c
+++ Exercise_4_2/src/main.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+#define FILE_PATH "~/Informatik-II-eclipse/Exercise_4_2/resources/hamlet.txt"
+
+// Datei hamlet.txt öffnen und Buchstaben getrennt nach Klein-/Großbuchstaben zählen
 
 int main(int argc, char *argv[]){
 
