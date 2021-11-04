a6145c8ec4389eda8846f5a8094e41383d0cd441
b-lind <himbeerhacks@outlook.de>
2021-11-04 10:32:28 +0100

    Finished Exercise 4.2, started Exercise 4.3

--- Exercise_4_2/src/main.c
+++ Exercise_4_2/src/main.c
@@ -1,10 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 
-#define FILE_PATH "~/Informatik-II-eclipse/Exercise_4_2/resources/hamlet.txt"
+#define FILE_PATH_IN "/home/bjoern/Informatik-II/eclipse/hamlet.txt"
+#define FILE_PATH_OUT "/home/bjoern/Informatik-II/eclipse/hamlet-result.txt"
 
 // Datei hamlet.txt öffnen und Buchstaben getrennt nach Klein-/Großbuchstaben zählen
+// Ergbenis in Datei schreiben
 
 int main(int argc, char *argv[]){
+    FILE *fInput;
+    FILE *fOutput;
+    
+    int iUppers[26];
+    int iLowers[26];
 
+    memset(iUppers, 0, sizeof(iUppers));
+    memset(iLowers, 0, sizeof(iLowers));
+
+    fInput = fopen(FILE_PATH_IN, "r");
+
+    if(fInput == NULL) {
+        printf("Fehler beim Öffnen der Datei.\n");
+        return -1;
+    }
+    
+    char cChar;
+        
+    do {
+        cChar = fgetc(fInput);
+
+        if(cChar >= 65 && cChar <= 90)
+            iUppers[cChar - 65] += 1;
+        else if (cChar >= 97 && cChar <= 122)
+            iLowers[cChar - 97] += 1;
+
+    } while (cChar != EOF);
+
+    fclose(fInput);
+
+    fOutput = fopen(FILE_PATH_OUT, "w");
+
+    if(fOutput == NULL) {
+        printf("Fehler beim Öffnen der Ergebnisdatei. Fallback zu Standardausgabe.\n");
+        fOutput = stdout;
+    }
+
+    int i;
+    for(i = 0; i < 26; i++) {
+        fprintf(fOutput, "%c: %d \t %c: %d\n", (char)(i+65), *(iUppers+i), (char)(i+97), *(iLowers+i));
+    }
+
+    fclose(fOutput);
 }
---
+++ Exercise_4_3/src/ComplexNumbers.c
@@ -0,0 +1,70 @@
+﻿#include <ComplexNumbers.h>
+
+// Allocate Memory for num complex numbers
+ComplexNumber_t *ComplexNumber_create(int num) {
+    return (ComplexNumber_t*)calloc(num, sizeof(ComplexNumber_t));
+}
+
+// Free Memory
+void ComplexNumber_delete(ComplexNumber_t *pComplexNumers) {
+    free(pComplexNumers);
+}
+
+
+// Set Real and Imaginary Part
+void ComplexNumber_setCartesian(ComplexNumber_t *pComplexNumer,
+			double dRealPart,
+			double dImaginaryPart) {
+    
+    pComplexNumer->dRealPart        = dRealPart;
+    pComplexNumer->dImaginaryPart   = dImaginaryPart;
+}
+
+// Get Real and Imaginary Part
+int ComplexNumber_getCartesianParameters(ComplexNumber_t *pComplexNumer,
+			double *pdRealPart,
+			double *pdImaginaryPart) {
+    
+    *pdRealPart         = pComplexNumer->dRealPart;
+    *pdImaginaryPart    = pComplexNumer->dImaginaryPart;
+}
+
+// Get Polar representation of complex number
+int ComplexNumber_getPolarParameters(ComplexNumber_t *pComplexNumer,
+			double *pdRadius,
+			double *pdAngle) {
+
+}
+
+
+// Add two complex numbers
+int ComplexNumber_add(ComplexNumber_t *pComplexNumerResult,
+			ComplexNumber_t *pComplexNumerOne,
+			ComplexNumber_t *pComplexNumerTwo) {
+
+    pComplexNumerResult->dRealPart      = pComplexNumerOne->dRealPart       + pComplexNumerTwo->dRealPart;
+    pComplexNumerResult->dImaginaryPart = pComplexNumerOne->dImaginaryPart  + pComplexNumerTwo->dImaginaryPart;
+}
+
+// Subtract two complex numbers
+int ComplexNumber_sub(ComplexNumber_t *pComplexNumerResult,
+			ComplexNumber_t *pComplexNumerOne,
+			ComplexNumber_t *pComplexNumerTwo) {
+    
+    pComplexNumerResult->dRealPart      = pComplexNumerOne->dRealPart       - pComplexNumerTwo->dRealPart;
+    pComplexNumerResult->dImaginaryPart = pComplexNumerOne->dImaginaryPart  - pComplexNumerTwo->dImaginaryPart;
+}
+
+// Multiply two complex numbers
+int ComplexNumber_mul(ComplexNumber_t *pComplexNumerResult,
+			ComplexNumber_t *pComplexNumerOne,
+			ComplexNumber_t *pComplexNumerTwo) {
+
+}
+
+// // Divide two complex numbers
+int ComplexNumber_div(ComplexNumber_t *pComplexNumerResult,
+			ComplexNumber_t *pComplexNumerOne,
+			ComplexNumber_t *pComplexNumerTwo) {
+
+}
